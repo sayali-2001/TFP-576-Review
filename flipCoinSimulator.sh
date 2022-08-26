346f9ec78b41833f4e359b02900d6fea5632b489
sayali-2001 <sayalithakare47@gmail.com>
2022-08-26 20:09:52 +0530

    Added : Modificati

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -8,3 +8,20 @@ then
 else 
 	echo "Tails";
 fi
+
+echo"-----------------------------";
+
+head=0;
+tail=0;
+
+for(( i=0 ;i<10 ; i++))
+do
+  if [ $flip -eq 1 ]
+  then 
+  	head=$((head+1));
+      echo "Heads $head";
+  else
+      tail=$((tail+1));
+      echo "Tails $tail";
+  fi
+done
